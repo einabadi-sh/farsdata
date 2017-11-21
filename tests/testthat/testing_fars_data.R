c44ac93fc3424e32e984920bfe4d802ac64742cc
Shahab Einabadi <sh.einabadi@yahoo.com>
2017-11-20 21:43:04 -0500

    modify the test

--- tests/testthat/testing_fars_data.R
+++ tests/testthat/testing_fars_data.R
@@ -4,15 +4,9 @@ test_that('Simple test', {
   testthat::expect_that(make_filename(2013), equals("accident_2013.csv.bz2"))
 })
 
-
 test_that("import the fars data", {
-  olddir <- getwd()
-  file="accident_2013.csv.bz2"
-  data_file<-system.file("extdata",file, package = "farsdata")
-  setwd(dirname(data_file))
-  dat<-fars_read(file)
-  setwd(olddir)
-  expect_that(dat, is_a('tbl_df'))
-  expect_equal(nrow(dat), 30202)
+  data_path <- system.file("extdata", "accident_2013.csv.bz2", package = "farsdata")
+  dat       <- fars_read(data_path)
+  testthat::expect_that(dat, is_a('tbl_df'))
+  testthat::expect_equal(nrow(dat), 30202)
 })
-
